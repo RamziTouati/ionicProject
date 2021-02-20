6ea0611ae22d8cda46ed3f22863b0c549ab8e190
Ramzi Touati <rtouati@diginov.tech>
2021-02-20 12:21:52 +0100

    side bar navigation

--- src/app/core/navbar/navbar.page.ts
+++ src/app/core/navbar/navbar.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-navbar',
@@ -8,21 +7,7 @@ import { MenuController } from '@ionic/angular';
 })
 export class NavbarPage implements OnInit {
 
-  constructor(private menu: MenuController) { }
-
-  openFirst() {
-    this.menu.enable(true, 'first');
-    this.menu.open('first');
-  }
-
-  openEnd() {
-    this.menu.open('end');
-  }
-
-  openCustom() {
-    this.menu.enable(true, 'custom');
-    this.menu.open('custom');
-  }
+ 
 
   ngOnInit() {
   }
